7ac3eb2e25261c0298c0c8e0c28bf8225bf5f51b
Leonardo Ragogna <laragogna@icloud.com>
2026-06-01 18:23:06 -0700

    more factors + traverse raw data

--- src/factor_generator.py
+++ src/factor_generator.py
@@ -8,27 +8,43 @@ raw_folder = project_root / "data" / "raw"
 factors_folder = project_root / "data" / "factors"
 
 
+def get_factors(df):
+    #calculate 30 and 90 day moment --> end/start - 1
+    df["momentum_30"] = (df["Close"] / df["Close"].shift(30)) - 1
+    df["momentum_90"] = (df["Close"] / df["Close"].shift(90)) - 1
 
-#create dataframe to read csv data
-df = pd.read_csv(raw_folder / "AAPL.csv")
+    #calculate volatility by std dev of 1 day momentum over X day time span
+    one_day_momentums = (df["Close"] / df["Close"].shift(1)) - 1
+    df["volatility_30"] = one_day_momentums.rolling(30).std()
+    df["volatility_90"] = one_day_momentums.rolling(90).std()
 
-#calculate 30 and 90 day moment --> end/start - 1
-df["momentum_30"] = (df["Close"] / df["Close"].shift(30)) - 1
-df["momentum_90"] = (df["Close"] / df["Close"].shift(90)) - 1
+    #calculate moving average distance (how far stock moves from average over X day time span, relative percentage)
+    df["moving_average_30"] = df["Close"] / df["Close"].rolling(30).mean() - 1
+    df["moving_average_90"] = df["Close"] / df["Close"].rolling(90).mean() - 1
 
-#calculate volatility by std dev of 1 day momentum over X day time span
-one_day_momentums = (df["Close"] / df["Close"].shift(1)) - 1
-df["volatility_30"] = one_day_momentums.rolling(30).std()
-df["volatility_90"] = one_day_momentums.rolling(90).std()
+    #calculate volume ratio (relative percentage) of X day time span 
+    df["volume_ratio_30"] = df["Volume"] / df["Volume"].rolling(30).mean() - 1
+    df["volume_ratio_90"] = df["Volume"] / df["Volume"].rolling(90).mean() - 1
+
+    return df[[
+        "Date",
+        "momentum_30",
+        "momentum_90",
+        "volatility_30",
+        "volatility_90",
+        "moving_average_30",
+        "moving_average_90",
+        "volume_ratio_30",
+        "volume_ratio_90"
+    ]]
 
 
-momentum_df = df[[
-    "Date",
-    "momentum_30",
-    "momentum_90",
-    "volatility_30",
-    "volatility_90",
-]]
+'''
+create dataframe to read csv data for each data file in raw_folder
+'''
+for file in raw_folder.glob("*.csv"):
+    symbol = file.stem
+    df = pd.read_csv(file)
+    factor_df = get_factors(df)
 
-
-momentum_df.to_csv(factors_folder / "AAPL_factors.csv", index = False)
+    factor_df.to_csv(factors_folder / f"{symbol}_factors.csv", index = False)
